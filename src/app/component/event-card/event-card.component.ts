d9f4e6049849a02db86f102eb4bd08c53d277553
Jam <107287729+Jam-is-Coding@users.noreply.github.com>
2025-08-05 11:01:16 +0000

    Co-authored-by: Leonard develops <leonard-roepcke@users.noreply.github.com>

--- src/app/component/event-card/event-card.component.ts
+++ src/app/component/event-card/event-card.component.ts
@@ -6,6 +6,8 @@ import { CommonModule } from '@angular/common'
   standalone: true,
   imports: [CommonModule],
   templateUrl: './event-card.component.html',
+  styleUrl: '/event-card.component.scss',
 })
 export class EventCardComponent {
 }
+
