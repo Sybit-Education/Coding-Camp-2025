cfb3f762acf6dc9e9d76c54811c6cae2548a559a
yblacky <schwarz.yannik@online.de>
2025-08-05 14:17:58 +0200

    feat: map

--- src/app/pages/event-detail/event-detail.component.ts
+++ src/app/pages/event-detail/event-detail.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core'
+import { MapComponent } from '../../component/map/map.component'
 @Component({
   selector: 'app-event-detail-page',
   standalone: true,
-  imports: [],
-  styleUrl: "./event-detail.component.scss",
+  imports: [MapComponent],
+  styleUrl: './event-detail.component.scss',
   templateUrl: './event-detail.component.html',
 })
-export class EventDetailPageComponent {
-}
+export class EventDetailPageComponent {}
