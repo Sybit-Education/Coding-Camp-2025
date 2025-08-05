6ed11589f2fcbca327fe29647e96544ca54b202a
Jam <107287729+Jam-is-Coding@users.noreply.github.com>
2025-08-05 09:57:25 +0000

    feat:startpage-basic-layout

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core'
+import { EventCardComponent } from "../../component/event-card/event-card.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [EventCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
