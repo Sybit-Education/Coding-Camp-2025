0e886b60ced7be5b0d7107c04840ddfc162d3a85
adriano-cybit <adrianomestre2000@gmail.com>
2025-08-05 12:09:15 +0000

    feat: created event site

--- src/app/pages/event-detail/event-detail.component.ts
+++ src/app/pages/event-detail/event-detail.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core'
   selector: 'app-event-detail-page',
   standalone: true,
   imports: [],
+  styleUrl: "./event-detail.component.scss",
   templateUrl: './event-detail.component.html',
 })
 export class EventDetailPageComponent {
