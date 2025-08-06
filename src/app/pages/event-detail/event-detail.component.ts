c94d987a71adaa159fe98a297a14afabc8bd9ba5
adriano-cybit <adrianomestre2000@gmail.com>
2025-08-06 11:44:04 +0000

    feat: added Calendar button - fixed some text

--- src/app/pages/event-detail/event-detail.component.ts
+++ src/app/pages/event-detail/event-detail.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core'
+import { Component, EventEmitter, Output } from '@angular/core'
 import { MapComponent } from '../../component/map/map.component'
 import { CalenderComponent } from '../../component/calender/calendar.component'
 import { AboutComponent } from '../about/about.component'
+import { SaveEventButtonComponent } from "../../component/save-event-button/save-event-button.component";
 
 @Component({
   selector: 'app-event-detail-page',
   standalone: true,
-  imports: [MapComponent, CalenderComponent, AboutComponent],
+  imports: [MapComponent, CalenderComponent, AboutComponent, SaveEventButtonComponent],
   styleUrl: './event-detail.component.scss',
   templateUrl: './event-detail.component.html',
 })
-export class EventDetailPageComponent {}
+export class EventDetailPageComponent {
+
+}
+
