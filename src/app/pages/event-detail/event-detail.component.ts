31846ef943c5d01582450423fbd37a4dceb45bab
adriano-cybit <adrianomestre2000@gmail.com>
2025-08-06 07:53:28 +0000

    feat: event detail

--- src/app/pages/event-detail/event-detail.component.ts
+++ src/app/pages/event-detail/event-detail.component.ts
@@ -3,10 +3,12 @@ import { MapComponent } from '../../component/map/map.component'
 import { CalenderComponent } from '../../component/calender/calendar.component'
 import { AboutComponent } from "../about/about.component";
 
+
+
 @Component({
   selector: 'app-event-detail-page',
   standalone: true,
-  imports: [MapComponent, CalenderComponent, AboutComponent],
+  imports: [MapComponent, CalenderComponent, AboutComponent, ],
   styleUrl: './event-detail.component.scss',
   templateUrl: './event-detail.component.html',
 })
