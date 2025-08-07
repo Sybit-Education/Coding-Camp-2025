1509847b7cfe68993ec86d68876a9d6678c25162
Leonard develops <120578632+leonard-roepcke@users.noreply.github.com>
2025-08-07 14:25:17 +0000

    feat: init categorie

---
+++ src/app/pages/kategorie/kategorie.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kategorie',
+  imports: [],
+  templateUrl: './kategorie.component.html',
+  styleUrl: './kategorie.component.scss'
+})
+export class KategorieComponent {
+
+}
