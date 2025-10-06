512104588e89fc04e2b89040cf6c27520152f641
Strittmatter, Stephan <Stephan.Strittmatter@sybit.de>
2025-10-06 12:12:39 +0200

    fix: staging config corrected

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,6 +1,7 @@
 export const environment = {
+  production: true,
   surrealDbAddress: 'wss://surrealdb.sybit.education',
   surrealDbNamespace: '1200.sybit.education',
-  surrealDbDatabase: 'main',
+  surrealDbDatabase: 'staging',
   MEDIA_BASE_URL: 'https://1200-jahre-radolfzell.sybit.education/media/',
 }
