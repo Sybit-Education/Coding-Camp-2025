3dc10ee23724ca2379009dbba306c7e8db79101d
Strittmatter, Stephan <Stephan.Strittmatter@sybit.de>
2025-10-07 11:58:50 +0200

    fix: import environment corrected

--- src/app/services/surrealdb.service.ts
+++ src/app/services/surrealdb.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core'
 import Surreal, { RecordId, StringRecordId, Token } from 'surrealdb'
-import { environment } from '../../environments/environment.production'
+import { environment } from '../../environments/environment'
 
 @Injectable({
   providedIn: 'root',
