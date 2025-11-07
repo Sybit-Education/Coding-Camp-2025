6c55b8aeb40a78dcb78a68034a68f2da88e40b03
yblacky <schwarz.yannik@online.de>
2025-11-07 16:04:19 +0100

    fix: back button

--- src/app/component/go-back-button/go-back-button.component.ts
+++ src/app/component/go-back-button/go-back-button.component.ts
@@ -1,6 +1,6 @@
 import { Location } from '@angular/common'
-import { Component, inject, Input } from '@angular/core'
-import { NavigationExtras, Router, RouterModule } from '@angular/router'
+import { Component, inject } from '@angular/core'
+import { RouterModule } from '@angular/router'
 import { IconComponent } from '@app/icons/icon.component'
 import { TranslateModule } from '@ngx-translate/core'
 
@@ -11,21 +11,9 @@ import { TranslateModule } from '@ngx-translate/core'
   templateUrl: './go-back-button.component.html',
 })
 export class GoBackComponent {
-  @Input() siteToGo?: string | string[]
-  @Input() queryParams?: Record<string, string | number | boolean | null | undefined>
-  @Input() label? = 'zur Home Seite'
-
-  private readonly router = inject(Router)
   private readonly location = inject(Location)
 
   goBack(): void {
-    if (this.siteToGo) {
-      const commands = Array.isArray(this.siteToGo) ? this.siteToGo : [this.siteToGo]
-      const extras: NavigationExtras = this.queryParams ? { queryParams: this.queryParams } : {}
-      this.router.navigate(commands, extras)
-    } else {
-      // Fallback
-      this.location.back()
-    }
+    this.location.back()
   }
 }
