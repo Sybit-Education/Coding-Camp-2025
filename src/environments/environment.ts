966b3c9896d72047b394e32a6cddfbda6abdb12e
Frank, Elias <Elias.Frank@sybit.de>
2025-12-10 10:40:49 +0100

    chore: cahnged db for show

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   production: false,
   surrealDbAddress: 'https://surrealdb.sybit.education',
   surrealDbNamespace: '1200.sybit.education',
-  surrealDbDatabase: 'develop',
+  surrealDbDatabase: 'main',
   MEDIA_BASE_URL: 'https://1200-jahre-radolfzell.sybit.education/media',
-  MEDIA_GET_SUFFIX: '/dev/'
+  MEDIA_GET_SUFFIX: '/'
 }
