df25259ae0fb9db2257b67389ea73a4f6563d0d0
yblacky <schwarz.yannik@online.de>
2026-06-19 13:48:07 +0200

    fix: removed deps error and aligned local db setup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,19 +1,9 @@
-// export const environment = {
-//   configName: 'local',
-//   production: false,
-//   surrealDbAddress: 'https://surrealdb.sybit.education',
-//   surrealDbNamespace: '1200.sybit.education',
-//   surrealDbDatabase: 'develop',
-//   MEDIA_BASE_URL: 'https://1200-jahre-radolfzell.sybit.education/media',
-//   MEDIA_GET_SUFFIX: '/dev/',
-// }
-
 export const environment = {
   configName: 'local',
-  production: false,
-  surrealDbAddress: 'http://localhost:9092',
+  production: true,
+  surrealDbAddress: 'https://surrealdb.sybit.education',
   surrealDbNamespace: '1200.sybit.education',
-  surrealDbDatabase: 'local',
-  MEDIA_BASE_URL: 'http://localhost:5240',
-  MEDIA_GET_SUFFIX: '/media/dev/',
+  surrealDbDatabase: 'develop',
+  MEDIA_BASE_URL: 'https://1200-jahre-radolfzell.sybit.education/media',
+  MEDIA_GET_SUFFIX: '/dev/',
 }
